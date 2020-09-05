f19fc647eba9306f314933f5d60c8876e12f72ff
Pawel Winek <pwinek@gmail.com>
2020-09-05 17:11:14 +0200

    Add managing articles

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 'use strict';
 
 function titleClickHandler(event){
+	event.preventDefault();
 	const clickedElement = this;
 	console.log('Link was clicked!');
 	/* [DONE] remove class 'active' from all article links */
+
 	const activeLinks = document.querySelectorAll('.titles a.active');
 
 	for(let activeLink of activeLinks){
 		activeLink.classList.remove('active');
 	}
-	/* [IN PROGRESS] add class 'active' to the clicked link */
+	/* [DONE] add class 'active' to the clicked link */
 
 	console.log('clickedElement:', clickedElement);
 	clickedElement.classList.add('active');
@@ -20,11 +22,15 @@ function titleClickHandler(event){
 	for(let activeArticle of activeArticles){
 		activeArticle.classList.remove('active');
 	}
-	/* get 'href' attribute from the clicked link */
+	/* [DONE] get 'href' attribute from the clicked link */
 
-	/* find the correct article using the selector (value of 'href' attribute) */
-
-	/* add class 'active' to the correct article */	
+	const articleSelector = clickedElement.getAttribute("href");
+	
+	/* [DONE] find the correct article using the selector (value of 'href' attribute) */
+	const targetArticle = document.querySelector(articleSelector);
+	
+	/* [DONE] add class 'active' to the correct article */	
+	targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll(' .titles a');
