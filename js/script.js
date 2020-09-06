7b6fc8ea0d1cf3734bc5f3b23f4aa41b77d8de9b
Pawel Winek <pwinek@gmail.com>
2020-09-06 15:52:11 +0200

    Add title generator

--- js/script.js
+++ js/script.js
@@ -33,8 +33,39 @@ function titleClickHandler(event){
 	targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll(' .titles a');
+/*generating title list*/
+const optArticleSelector = '.post',
+	optTitleSelector = '.post-title',
+	optTitleListSelector = '.titles';
 
-for(let link of links){
-	link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(){
+
+	/* remove contents of titleList */
+	const titleList = document.querySelector(optTitleListSelector);
+	console.log(titleList);
+	titleList.innerHTML = '';
+	let html = '';
+	
+	/* for each article */
+	const articles = document.querySelectorAll(optArticleSelector);
+	for(let article of articles){
+		/* get the article id */
+		const articleId = article.getAttribute('id');
+		console.log(articleId);
+		/* find the title element and get title form title element */
+		const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+		console.log(articleTitle);
+		/* create HTML of the link */
+		const linkHTML = '<li><a href="#'+ articleId +'"><span>'+ articleTitle +'</span></a></li>';
+		console.log(linkHTML);
+		/* insert link into titleList */
+		html = titleList.insertAdjacentHTML('beforeend', linkHTML);
+	}
+	const links = document.querySelectorAll(' .titles a');
+	console.log(links);
+	for(let link of links){
+		link.addEventListener('click', titleClickHandler);
+	}
 }
+
+generateTitleLinks();
